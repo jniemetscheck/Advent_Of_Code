4af47ce456cd4dd9916fd2694119c1c968479843
Justin Niemetscheck <jniemetscheck@nationaldecisionsupport.com>
2017-12-12 14:31:25 -0600

    day 7 part 2 - not done, but completed the anser :(

--- AdventForCode2017/Days/Day07.cs
+++ AdventForCode2017/Days/Day07.cs
@@ -10,6 +10,100 @@ namespace AdventOfCode2017.Days
 
         public static string GetPart1Result()
         {
+            return GetResult().Bottom;
+        }
+
+        public static int GetPart2Result()
+        {
+            var results = GetResult();
+            var tower = results.Tower;
+
+            return DoBalanceCheck(tower, results.Bottom).WeightNeeded;
+        }
+
+        private static (bool IsImbalanced, int WeightNeeded) DoBalanceCheck(Tower tower, string bottomName)
+        {
+            var bottom = tower.Floors.FirstOrDefault(x => x.Name == bottomName);
+            var weights = new Dictionary<int, List<string>>();
+
+            if (bottom.FloorsAbove != null && bottom.FloorsAbove.Any())
+            {
+                foreach (var floor in bottom.FloorsAbove)
+                {
+                    var currentFloor = tower.Floors.FirstOrDefault(x => x.Name == floor.Name);
+
+                    var floorsAboveWeight = GetWeight(tower, floor.Name);
+
+                    if (!weights.ContainsKey(currentFloor.Weight + floorsAboveWeight))
+                    {
+                        weights.Add(currentFloor.Weight + floorsAboveWeight, new List<string>() { currentFloor.Name });
+                    }
+                    else
+                    {
+                        weights[currentFloor.Weight + floorsAboveWeight].Add(currentFloor.Name);
+                    }
+                }
+
+                if (weights.Count > 1 && weights.Any(x => x.Value.Count == 1))
+                {
+                    //imbalanced
+                    var goodWeight = weights.FirstOrDefault(y => y.Value.Count > 1).Key;
+
+                    if (goodWeight > bottom.Weight)
+                    {
+                        //we're under weight
+                        bottom.Weight = bottom.Weight + (goodWeight - bottom.Weight);
+                    }
+                    else
+                    {
+                        //we're over weight
+                        bottom.Weight = bottom.Weight - (bottom.Weight - goodWeight);
+                    }
+
+                    //if we're bad, we need to check to see which one is actually imbalanced - recursion
+
+                    return DoBalanceCheck(tower, weights.FirstOrDefault(y => y.Value.Count == 1).Value[0]);
+                }
+                else
+                {
+                    foreach (var floor in bottom.FloorsAbove)
+                    {
+                        var check = DoBalanceCheck(tower, floor.Name);
+
+                        if (check.IsImbalanced)
+                        {
+                            return (true, check.WeightNeeded);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                return (false, 0);
+            }
+
+            return (true, 0);
+        }
+
+        private static int GetWeight(Tower tower, string bottomName)
+        {
+            var currentFloor = tower.Floors.FirstOrDefault(x => x.Name == bottomName);
+            var totalWeight = 0;
+
+            if (currentFloor.FloorsAbove != null && currentFloor.FloorsAbove.Any())
+            {
+                foreach (var floorAbove in currentFloor.FloorsAbove)
+                {
+                    var floor = tower.Floors.FirstOrDefault(x => x.Name == floorAbove.Name);
+                    totalWeight += GetWeight(tower, floorAbove.Name) + floor.Weight;
+                }
+            }
+
+            return totalWeight;
+        }
+
+        private static (string Bottom, Tower Tower) GetResult()
+        {
             var lowestFloor = string.Empty;
             var tower = GetTower();
             var floorsWithOtherFloors = tower.Floors.Where(f => f.FloorsAbove != null && f.FloorsAbove.Count > 0);
@@ -19,7 +113,7 @@ namespace AdventOfCode2017.Days
                 var outerExists = false;
                 foreach (var innerFloor in floorsWithOtherFloors)
                 {
-                    
+
                     //skip current floor
                     if (innerFloor.Name != floor.Name)
                     {
@@ -45,8 +139,9 @@ namespace AdventOfCode2017.Days
                 }
             }
 
-            return lowestFloor;
+            return (lowestFloor, tower);
         }
+
 
         private static Tower GetTower()
         {
@@ -93,6 +188,7 @@ namespace AdventOfCode2017.Days
             public string Name { get; set; }
             public int Weight { get; set; }
             public List<Floor> FloorsAbove { get; set; }
+            //public int TotalWeight { get; set; }
         }
     }
 }
--- AdventForCode2017/Program.cs
+++ AdventForCode2017/Program.cs
@@ -6,30 +6,30 @@ namespace AdventOfCode2017
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Result for Day 01-Part 1: {Days.Day01.GetPart1Result()}");
-            Console.WriteLine($"Result for Day 01-Part 2: {Days.Day01.GetPart2Result()}");
-            Console.WriteLine();
-            Console.WriteLine($"Result for Day 02-Part 1: {Days.Day02.GetPart1Result()}");
-            Console.WriteLine($"Result for Day 02-Part 2: {Days.Day02.GetPart2Result()}");
-            Console.WriteLine();
-            Console.WriteLine($"Result for Day 03-Part 1: {Days.Day03.GetPart1Result()}");
-            Console.WriteLine($"Result for Day 03-Part 2: {Days.Day03.GetPart2Result()}");
-            Console.WriteLine();
-            Console.WriteLine($"Result for Day 04-Part 1: {Days.Day04.GetPart1Result()}");
-            Console.WriteLine($"Result for Day 04-Part 2: {Days.Day04.GetPart2Result()}");
-            Console.WriteLine();
-            Console.WriteLine($"Result for Day 05-Part 1: {Days.Day05.GetPart1Result()}");
-            Console.WriteLine($"Result for Day 05-Part 2: {Days.Day05.GetPart2Result()}");
-            Console.WriteLine();
-            Console.WriteLine($"Result for Day 06-Part 1: {Days.Day06.GetPart1Result()}");
-            Console.WriteLine($"Result for Day 06-Part 2: {Days.Day06.GetPart2Result()}");
-            Console.WriteLine();
-            Console.WriteLine($"Result for Day 07-Part 1: {Days.Day07.GetPart1Result()}");
-            //Console.WriteLine($"Result for Day 07-Part 2: {Days.Day07.GetPart2Result()}");
-            Console.WriteLine();
-            var day08Result = Days.Day08.GetResult();
-            Console.WriteLine($"Result for Day 08-Part 1: {day08Result.Part1}");
-            Console.WriteLine($"Result for Day 08-Part 1: {day08Result.Part2}");
+            //Console.WriteLine($"Result for Day 01-Part 1: {Days.Day01.GetPart1Result()}");
+            //Console.WriteLine($"Result for Day 01-Part 2: {Days.Day01.GetPart2Result()}");
+            //Console.WriteLine();
+            //Console.WriteLine($"Result for Day 02-Part 1: {Days.Day02.GetPart1Result()}");
+            //Console.WriteLine($"Result for Day 02-Part 2: {Days.Day02.GetPart2Result()}");
+            //Console.WriteLine();
+            //Console.WriteLine($"Result for Day 03-Part 1: {Days.Day03.GetPart1Result()}");
+            //Console.WriteLine($"Result for Day 03-Part 2: {Days.Day03.GetPart2Result()}");
+            //Console.WriteLine();
+            //Console.WriteLine($"Result for Day 04-Part 1: {Days.Day04.GetPart1Result()}");
+            //Console.WriteLine($"Result for Day 04-Part 2: {Days.Day04.GetPart2Result()}");
+            //Console.WriteLine();
+            //Console.WriteLine($"Result for Day 05-Part 1: {Days.Day05.GetPart1Result()}");
+            //Console.WriteLine($"Result for Day 05-Part 2: {Days.Day05.GetPart2Result()}");
+            //Console.WriteLine();
+            //Console.WriteLine($"Result for Day 06-Part 1: {Days.Day06.GetPart1Result()}");
+            //Console.WriteLine($"Result for Day 06-Part 2: {Days.Day06.GetPart2Result()}");
+            //Console.WriteLine();
+            //Console.WriteLine($"Result for Day 07-Part 1: {Days.Day07.GetPart1Result()}");
+            Console.WriteLine($"Result for Day 07-Part 2: {Days.Day07.GetPart2Result()}");
+            //Console.WriteLine();
+            //var day08Result = Days.Day08.GetResult();
+            //Console.WriteLine($"Result for Day 08-Part 1: {day08Result.Part1}");
+            //Console.WriteLine($"Result for Day 08-Part 1: {day08Result.Part2}");
 
             Console.ReadLine();
         }
