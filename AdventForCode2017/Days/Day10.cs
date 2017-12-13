3973ea7a2dc3d5663016a768c69f30bf691333b7
Justin Niemetscheck <jniemetscheck@nationaldecisionsupport.com>
2017-12-13 17:48:09 -0600

    day 10 - not finished

---
+++ AdventForCode2017/Days/Day10.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2017.Days
+{
+    public static class Day10
+    {
+        public static int GetPart1Result()
+        {
+            var input = new List<int> { 225, 171, 131, 2, 35, 5, 0, 13, 1, 246, 54, 97, 255, 98, 254, 110 };
+            return GetResult(256, input);
+        }
+
+        public static int GetPart1Result(int listCount, List<int> lengths)
+        {
+            return GetResult(listCount, lengths);
+        }
+
+        public static int GetResult(int listCount, List<int> lengths)
+        {
+            var list = new List<int>();
+            for (int i = 0; i < listCount; i++)
+            {
+                list.Add(i);
+            }
+
+            var currentPosition = 0;
+            var skipSize = 0;
+            for (int i = 0; i < lengths.Count; i++)
+            {
+                var length = lengths[i];
+
+                if (length + currentPosition < listCount)
+                {
+                    //not wrapping
+                    var temp = new List<int>();
+                    for (int j = currentPosition; j < length; j++)
+                    {
+                        temp.Add(list[j]);
+                    }
+                    var reversed = GetReversedList(temp);
+                    var reversedPosition = 0;
+                    for (int k = currentPosition; k < length; k++)
+                    {
+                        list[k] = reversed[reversedPosition];
+                        reversedPosition++;
+                    }
+
+                    if (skipSize + currentPosition < listCount)
+                    {
+                        currentPosition = lengths[i] + skipSize;
+                    }
+                    else
+                    {
+                        currentPosition = skipSize + currentPosition + 1 - listCount;
+                    }
+                }
+                else
+                {
+                    //wrapping
+                    var amountLeft = currentPosition + length - list.Count;
+                    var temp = new List<int>();
+                    for (int j = currentPosition; j < list.Count; j++)
+                    {
+                        temp.Add(list[j]);
+                    }
+                    for (int k = 0; k < amountLeft; k++)
+                    {
+                        temp.Add(list[k]);
+                    }
+                    var reversed = GetReversedList(temp);
+                    var reversedPosition = 0;
+                    for (int k = currentPosition; k < list.Count; k++)
+                    {
+                        list[k] = reversed[reversedPosition];
+                        reversedPosition++;
+                    }
+                    for (int k = 0; k < amountLeft; k++)
+                    {
+                        list[k] = reversed[reversedPosition];
+                        reversedPosition++;
+                    }
+                    currentPosition = amountLeft + skipSize;
+                }
+
+                skipSize++;
+            }
+
+            return list[0] * list[1];
+        }
+
+        private static List<int> GetReversedList(List<int> listToReverse)
+        {
+            var reversedList = new List<int>();
+
+            for (int i = (listToReverse.Count - 1); i >= 0; i--)
+            {
+                reversedList.Add(listToReverse[i]);
+            }
+
+            return reversedList;
+        }
+    }
+}
---
+++ AdventForCode2017/Tests/Day10Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace AdventOfCode2017.Tests
+{
+    [TestClass]
+    public class Day10Tests
+    {
+        [TestMethod]
+        public void TestOne()
+        {
+            var input = new List<int> { 3, 4, 1, 5 };
+            Assert.AreEqual(12, Days.Day10.GetPart1Result(5, input));
+        }
+
+        //[TestMethod]
+        //public void TestTwo()
+        //{
+        //    var input = new List<int> { 7, 4, 8, 9 };
+        //    Assert.AreEqual(12, Days.Day10.GetPart1Result(10, input));
+        }
+    }
+}
