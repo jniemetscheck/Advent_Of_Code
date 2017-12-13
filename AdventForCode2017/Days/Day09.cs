5f1ac8127086e4d9e2425a2c0f82aac550c342fb
Justin Niemetscheck <jniemetscheck@nationaldecisionsupport.com>
2017-12-12 20:12:26 -0600

    day 9

---
+++ AdventForCode2017/Days/Day09.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2017.Days
+{
+    public static class Day09
+    {
+        private static string FilePath = Directory.GetCurrentDirectory() + @"/Input/Day09.txt";
+
+        public static int GetPart1Result()
+        {
+            var stream = File.ReadAllText(FilePath);
+
+            return GetResult(stream.Select(c => c.ToString()).ToList()).Score;
+        }
+
+        public static int GetPart2Result()
+        {
+            var stream = File.ReadAllText(FilePath);
+
+            return GetResult(stream.Select(c => c.ToString()).ToList()).GarbageCount;
+        }
+
+        public static (int Score, int GarbageCount) GetResult(List<string> stream)
+        {
+            var score = 0;
+            var groupsStarted = 0;
+            var groupMultiplier = 0;
+            var ignoring = false;
+            var isCancelled = false;
+            var garbageCount = 0;
+
+            foreach (var item in stream)
+            {
+                if (isCancelled)
+                {
+                    isCancelled = false;
+                    continue;
+                }
+                if (item == "!")
+                {
+                    isCancelled = true;
+                    continue;
+                }
+                if (item == "<" || item == ">")
+                {
+                    if (item == "<")
+                    {
+                        if (!ignoring)
+                        {
+                            ignoring = true;
+                        }
+                        else
+                        {
+                            garbageCount++;
+                        }
+                    }
+                    else
+                    {
+                        if (!isCancelled)
+                        {
+                            ignoring = false;
+                        }
+                    }
+                    continue;
+                }
+
+                switch (item)
+                {
+                    case "{":
+                        if (!ignoring)
+                        {
+                            groupsStarted++;
+                            groupMultiplier++;
+                        }
+                        else
+                        {
+                            garbageCount++;
+                        }
+                        break;
+                    case "}":
+                        if (!ignoring)
+                        {
+                            groupsStarted--;
+                            score += groupMultiplier;
+                            groupMultiplier--;
+
+                        }
+                        else
+                        {
+                            garbageCount++;
+                        }
+                        break;
+                    case "<":
+                        ignoring = true;
+                        break;
+                    case ">":
+                        ignoring = false;
+                        break;
+                    default:
+                        if (ignoring)
+                        {
+                            garbageCount++;
+                        }
+                        break;
+                }
+            }
+
+            return (score, garbageCount);
+        }
+    }
+}
---
+++ AdventForCode2017/Tests/Day09Tests.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace AdventOfCode2017.Tests
+{
+    [TestClass]
+    public class Day09Tests
+    {
+        [TestMethod]
+        public void Part1_Test_One()
+        {
+            var result = Days.Day09.GetResult("{}".Select(c => c.ToString()).ToList());
+
+            Assert.AreEqual(1, result.Score);
+        }
+
+        [TestMethod]
+        public void Part1_Test_Two()
+        {
+            var result = Days.Day09.GetResult("{{{}}}".Select(c => c.ToString()).ToList());
+
+            Assert.AreEqual(6, result.Score);
+        }
+
+        [TestMethod]
+        public void Part1_Test_Three()
+        {
+            var result = Days.Day09.GetResult("{{},{}}".Select(c => c.ToString()).ToList());
+
+            Assert.AreEqual(5, result.Score);
+        }
+
+        [TestMethod]
+        public void Part1_Test_Four()
+        {
+            var result = Days.Day09.GetResult("{{{},{},{{}}}}".Select(c => c.ToString()).ToList());
+
+            Assert.AreEqual(16, result.Score);
+        }
+
+        [TestMethod]
+        public void Part1_Test_Five()
+        {
+            var result = Days.Day09.GetResult("{<a>,<a>,<a>,<a>}".Select(c => c.ToString()).ToList());
+
+            Assert.AreEqual(1, result.Score);
+        }
+
+        [TestMethod]
+        public void Part1_Test_Six()
+        {
+            var result = Days.Day09.GetResult("{{<ab>},{<ab>},{<ab>},{<ab>}}".Select(c => c.ToString()).ToList());
+
+            Assert.AreEqual(9, result.Score);
+        }
+
+        [TestMethod]
+        public void Part1_Test_Seven()
+        {
+            var result = Days.Day09.GetResult("{{<!!>},{<!!>},{<!!>},{<!!>}}".Select(c => c.ToString()).ToList());
+
+            Assert.AreEqual(9, result.Score);
+        }
+
+        [TestMethod]
+        public void Part1_Test_Eight()
+        {
+            var result = Days.Day09.GetResult("{{<a!>},{<a!>},{<a!>},{<ab>}}".Select(c => c.ToString()).ToList());
+
+            Assert.AreEqual(3, result.Score);
+        }
+
+        [TestMethod]
+        public void Part2_Test_One()
+        {
+            var result = Days.Day09.GetResult("<>".Select(c => c.ToString()).ToList());
+
+            Assert.AreEqual(0, result.GarbageCount);
+        }
+
+        [TestMethod]
+        public void Part2_Test_Two()
+        {
+            var result = Days.Day09.GetResult("<random characters>".Select(c => c.ToString()).ToList());
+
+            Assert.AreEqual(17, result.GarbageCount);
+        }
+
+        [TestMethod]
+        public void Part2_Test_Three()
+        {
+            var result = Days.Day09.GetResult("<<<<>".Select(c => c.ToString()).ToList());
+
+            Assert.AreEqual(3, result.GarbageCount);
+        }
+
+        [TestMethod]
+        public void Part2_Test_Four()
+        {
+            var result = Days.Day09.GetResult("<{!>}>".Select(c => c.ToString()).ToList());
+
+            Assert.AreEqual(2, result.GarbageCount);
+        }
+
+        [TestMethod]
+        public void Part2_Test_Five()
+        {
+            var result = Days.Day09.GetResult("<!!>".Select(c => c.ToString()).ToList());
+
+            Assert.AreEqual(0, result.GarbageCount);
+        }
+
+        [TestMethod]
+        public void Part2_Test_Six()
+        {
+            var result = Days.Day09.GetResult("<!!!>>".Select(c => c.ToString()).ToList());
+
+            Assert.AreEqual(0, result.GarbageCount);
+        }
+
+        [TestMethod]
+        public void Part2_Test_Seven()
+        {
+            var result = Days.Day09.GetResult("<{o\"i!a,<{i<a>".Select(c => c.ToString()).ToList());
+            
+            Assert.AreEqual(10, result.GarbageCount);
+        }
+    }
+}
