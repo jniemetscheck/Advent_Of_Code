fefd46a1b1b37aaffe6f894e05a78d78fd1c12de
Justin Niemetscheck <jniemetscheck@nationaldecisionsupport.com>
2017-12-14 12:54:56 -0600

    day 12 part 1

--- AdventForCode2017/Days/Day13.cs
+++ AdventForCode2017/Days/Day13.cs
@@ -1,16 +1,131 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventOfCode2017.Days
 {
     public static class Day13
     {
+        private static string FilePath = Directory.GetCurrentDirectory() + @"/Input/Day13.txt";
+
         public static int GetPart1Result()
         {
-            return 0;
+            return GetResult(GetInput());
+        }
+
+        public static int GetResult(List<FirewallEntry> firewallEntries)
+        {
+            var picosecond = 0;
+            var filledFirewallEntries = GetFilledFirewallEntries(SetInitialState(firewallEntries));
+            var currentDepth = 0;
+            var caught = new List<FirewallEntry>();
+
+            for (int i = 1; i <= filledFirewallEntries.Count; i++)
+            {
+                foreach (var scannerAtBeginningEntry in filledFirewallEntries.Where(x => x.ScannerAtRange == 1 && x.Range > 0))
+                {
+                    if (scannerAtBeginningEntry.Depth == currentDepth)
+                    {
+                        //we've been caught
+                        caught.Add(scannerAtBeginningEntry);
+                        break;
+                    }
+                }
+                foreach (var entry in filledFirewallEntries)
+                {
+                    if (entry.Range > 0)
+                    {
+                        if (entry.MovingDown)
+                        {
+                            if (entry.ScannerAtRange < entry.Range)
+                            {
+                                entry.ScannerAtRange++;
+                            }
+                            else if (entry.ScannerAtRange == entry.Range)
+                            {
+                                entry.ScannerAtRange--;
+                                entry.MovingDown = false;
+                            }
+                        }
+                        else
+                        {
+                            if (entry.ScannerAtRange > 1)
+                            {
+                                entry.ScannerAtRange--;
+                            }
+                            else if (entry.ScannerAtRange == 1)
+                            {
+                                entry.ScannerAtRange++;
+                                entry.MovingDown = true;
+                            }
+                        }
+                    }
+                }
+                picosecond++;
+                currentDepth++;
+            }
+
+            var severity = 0;
+            foreach (var caughtEntry in caught)
+            {
+                severity += caughtEntry.Depth * caughtEntry.Range;
+            }
+            return severity;
+        }
+
+        private static List<FirewallEntry> SetInitialState(List<FirewallEntry> firewallEntries)
+        {
+            foreach (var entry in firewallEntries)
+            {
+                entry.ScannerAtRange = 1;
+            }
+
+            return firewallEntries;
+        }
+
+        private static List<FirewallEntry> GetFilledFirewallEntries(List<FirewallEntry> firewallEntries)
+        {
+            for (int i = 0; i < firewallEntries.OrderBy(f => f.Depth).LastOrDefault().Depth; i++)
+            {
+                if (firewallEntries.FirstOrDefault(e => e.Depth == i) == null)
+                {
+                    var filler = new FirewallEntry { Range = 0 };
+                    firewallEntries.Insert(i, filler);
+                }
+            }
+
+            return firewallEntries;
+        }
+
+        private static List<FirewallEntry> GetInput()
+        {
+            var entries = new List<FirewallEntry>();
+
+            var file = new StreamReader(FilePath);
+            string line;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                var lineSplit = line.Split(':');
+                var depth = int.Parse(lineSplit[0].Trim());
+                var range = int.Parse(lineSplit[1].Trim());
+                entries.Add(new FirewallEntry { Depth = depth, Range = range });
+            }
+
+            return entries;
+        }
+    }
+
+    public class FirewallEntry
+    {
+        public int Depth { get; set; }
+        public int Range { get; set; }
+        public int ScannerAtRange { get; set; }
+        public bool MovingDown { get; set; }
+
+        public FirewallEntry()
+        {
+            MovingDown = true;
         }
     }
 }
---
+++ AdventForCode2017/Tests/Day13Tests.cs
@@ -0,0 +1,22 @@
+﻿using AdventOfCode2017.Days;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace AdventOfCode2017.Tests
+{
+    [TestClass]
+    public class Day13Tests
+    {
+        [TestMethod]
+        public void TestOne()
+        {
+            var entries = new List<FirewallEntry>();
+            entries.Add(new FirewallEntry { Depth = 0, Range = 3 });
+            entries.Add(new FirewallEntry { Depth = 1, Range = 2 });
+            entries.Add(new FirewallEntry { Depth = 4, Range = 4 });
+            entries.Add(new FirewallEntry { Depth = 6, Range = 4 });
+
+            Assert.AreEqual(24, Day13.GetResult(entries));
+        }
+    }
+}
