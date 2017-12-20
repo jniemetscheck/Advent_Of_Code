1b37a7fa38fd5fcc363fc4945ebe95bb63169c6c
Justin Niemetscheck <jniemetscheck@nationaldecisionsupport.com>
2017-12-19 20:55:56 -0600

    day 19 part 1

---
+++ AdventForCode2017/Days/Day19.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2017.Days
+{
+    public static class Day19
+    {
+        private static string FilePath = Directory.GetCurrentDirectory() + @"/Input/Day19.txt";
+
+        public static string GetPart1Result()
+        {
+            return GetPart1Result(GetMap());
+        }
+
+        public static string GetPart1Result(List<List<string>> map)
+        {
+            var path = string.Empty;
+            var travelDirection = TravelDirection.Down;
+            var currentY = 0;
+            var currentX = map[currentY].IndexOf("|");
+
+            currentY++;
+            while (true)
+            {
+                var currentChar = map[currentY][currentX];
+                switch (currentChar)
+                {
+                    case "|":
+                        if (travelDirection == TravelDirection.Left || travelDirection == TravelDirection.Right)
+                        {
+                            if (travelDirection == TravelDirection.Left)
+                            {
+                                currentX--;
+                            }
+                            else
+                            {
+                                currentX++;
+                            }
+                        }
+                        else if (travelDirection == TravelDirection.Down)
+                        {
+                            currentY++;
+                        }
+                        else
+                        {
+                            currentY--;
+                        }
+                        break;
+                    case "-":
+                        if (travelDirection == TravelDirection.Up || travelDirection == TravelDirection.Down)
+                        {
+                            if (travelDirection == TravelDirection.Up)
+                            {
+                                currentY--;
+                            }
+                            else
+                            {
+                                currentY++;
+                            }
+                        }
+                        else if (travelDirection == TravelDirection.Left)
+                        {
+                            currentX--;
+                        }
+                        else
+                        {
+                            currentX++;
+                        }
+                        break;
+                    case "+":
+                        if (travelDirection == TravelDirection.Down || travelDirection == TravelDirection.Up)
+                        {
+                            if (currentX == 0)
+                            {
+                                travelDirection = TravelDirection.Right;
+                                currentX++;
+                            }
+                            else if (currentX == map[currentY].Count - 1)
+                            {
+                                travelDirection = TravelDirection.Left;
+                                currentX--;
+                            }
+                            else
+                            {
+                                if (map[currentY][currentX - 1] != " ")
+                                {
+                                    travelDirection = TravelDirection.Left;
+                                    currentX--;
+                                }
+                                else
+                                {
+                                    travelDirection = TravelDirection.Right;
+                                    currentX++;
+                                }
+                            }
+                        }
+                        else if (travelDirection == TravelDirection.Left || travelDirection == TravelDirection.Right)
+                        {
+                            if (currentY == 0)
+                            {
+                                travelDirection = TravelDirection.Down;
+                                currentY++;
+                            }
+                            else if (currentY == map.Count - 1)
+                            {
+                                travelDirection = TravelDirection.Up;
+                                currentY--;
+                            }
+                            else
+                            {
+                                if (map[currentY + 1][currentX] != " ")
+                                {
+                                    travelDirection = TravelDirection.Down;
+                                    currentY++;
+                                }
+                                else
+                                {
+                                    travelDirection = TravelDirection.Up;
+                                    currentY--;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (currentY == 0)
+                            {
+                                currentY++;
+                            }
+                            else if (currentY == map.Count - 1)
+                            {
+                                currentY--;
+                            }
+                            else
+                            {
+                                if (map[currentY + 1][currentX] != " ")
+                                {
+                                    travelDirection = TravelDirection.Down;
+                                }
+                                else
+                                {
+                                    travelDirection = TravelDirection.Up;
+                                }
+                            }
+                        }
+                        break;
+                    case " ":
+                        currentX = -1;
+                        break;
+                    default:
+                        //  letter, add to list
+                        path += currentChar;
+                        if (travelDirection == TravelDirection.Down)
+                        {
+                            currentY++;
+                        }
+                        else if (travelDirection == TravelDirection.Up)
+                        {
+                            currentY--;
+                        }
+                        else if (travelDirection == TravelDirection.Left)
+                        {
+                            currentX--;
+                        }
+                        else
+                        {
+                            currentX++;
+                        }
+                        break;
+                }
+
+                if (currentX < 0 || currentY < 0 || currentY >= map.Count || currentX >= map[currentY].Count)
+                {
+                    break;
+                }
+            }
+
+            return path;
+        }
+
+        private static List<List<string>> GetMap()
+        {
+            var map = new List<List<string>>();
+            var lines = File.ReadAllLines(FilePath).ToList();
+
+            foreach (var line in lines)
+            {
+                map.Add(line.Select(c => c.ToString()).ToList());
+            }
+
+            return map;
+        }
+    }
+
+    public enum TravelDirection
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+}
---
+++ AdventForCode2017/Tests/Day19Tests.cs
@@ -0,0 +1,28 @@
+﻿using AdventOfCode2017.Days;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2017.Tests
+{
+    [TestClass]
+    public class Day19Tests
+    {
+        [TestMethod]
+        public void TestOne()
+        {
+            var map = new List<List<string>>();
+            map.Add("    |         ".Select(c => c.ToString()).ToList());
+            map.Add("    |  +--+   ".Select(c => c.ToString()).ToList());
+            map.Add("    A  |  C   ".Select(c => c.ToString()).ToList());
+            map.Add("F---|----E|--+".Select(c => c.ToString()).ToList());
+            map.Add("    |  |  |  D".Select(c => c.ToString()).ToList());
+            map.Add("    +B-+  +--+".Select(c => c.ToString()).ToList());
+
+            Assert.AreEqual("ABCDEF", Day19.GetPart1Result(map));
+        }
+    }
+}
