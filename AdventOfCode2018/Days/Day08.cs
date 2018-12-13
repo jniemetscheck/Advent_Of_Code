964bd35363cc8501e4189d574907c644df439042
jniemetscheck <jniemetscheck@gmail.com>
2018-12-12 20:06:12 -0600

    Day 08

---
+++ AdventOfCode2018/Days/Day08.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace AdventOfCode2018.Days
+{
+    public static class Day08
+    {
+        public static int GetMetadataSum(string license)
+        {
+            var entries = license.Split(' ').Select(int.Parse).ToList(); ;
+
+            var nodes = entries[0];
+            var metadata = entries[1];
+            var currentIndex = 2;
+
+            for (int i = 0; i < nodes; i++)
+            {
+                var subNodes = entries[currentIndex];
+                currentIndex++;
+
+                var subMetadata = entries[currentIndex];
+                currentIndex++;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCode2018/Tests/Day08Tests.cs
@@ -0,0 +1,17 @@
+﻿using AdventOfCode2018.Days;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode2018.Tests
+{
+    [TestClass]
+    public class Day08Tests
+    {
+        [TestMethod]
+        public void Part1_TestOne()
+        {
+            var input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2";
+
+            Assert.AreEqual(138, Day08.GetMetadataSum(input));
+        }
+    }
+}
