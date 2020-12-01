f4b3e685f6c92744325524db4ad80942c9981168
jniemetscheck <jniemetscheck@gmail.com>
2020-12-01 04:44:07 -0600

    2019 Day 1 Part 1

---
+++ AdventOfCode2019/Days/Day01.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2019.Days
+{
+    public static class Day01
+    {
+        private static readonly string FilePath = Directory.GetCurrentDirectory() + @"/Input/Day01.txt";
+
+        public static int GetResultPartOne()
+        {
+            var lines = File.ReadAllLines(FilePath);
+
+            return GetFuelRequiredToLaunch(lines.ToList());
+        }
+
+        public static int GetFuelRequiredToLaunch(List<string> moduleMasses)
+        {
+            var mass = 0;
+
+            foreach (var moduleMass in moduleMasses)
+            {
+                var currentMass = int.Parse(moduleMass);
+
+                mass += (int)Math.Floor((decimal)currentMass / 3) - 2;
+            }
+
+            return mass;
+        }
+    }
+}
---
+++ AdventOfCode2019/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AdventOfCode2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Result for Day 01 Part 1: {Days.Day01.GetResultPartOne()}");
+            //Console.WriteLine($"Result for Day 01 Part 2: {Days.Day01.GetResultPartTwo()}");
+
+            Console.ReadLine();
+        }
+    }
+}
