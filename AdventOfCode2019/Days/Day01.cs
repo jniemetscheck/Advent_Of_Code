b82ff5e70ad6dd085c53ddfdad5827d77d513f7b
jniemetscheck <jniemetscheck@gmail.com>
2020-12-01 05:14:26 -0600

    2019 Day 1 Part 2

--- AdventOfCode2019/Days/Day01.cs
+++ AdventOfCode2019/Days/Day01.cs
@@ -16,18 +16,56 @@ namespace AdventOfCode2019.Days
             return GetFuelRequiredToLaunch(lines.ToList());
         }
 
+        public static int GetResultPartTwo()
+        {
+            var lines = File.ReadAllLines(FilePath);
+
+            return GetTotalFuelRequirements(lines.ToList());
+        }
+
         public static int GetFuelRequiredToLaunch(List<string> moduleMasses)
         {
             var mass = 0;
 
             foreach (var moduleMass in moduleMasses)
             {
-                var currentMass = int.Parse(moduleMass);
-
-                mass += (int)Math.Floor((decimal)currentMass / 3) - 2;
+                mass += GetFuelForMass(int.Parse(moduleMass));
             }
 
             return mass;
+        }
+
+        private static int GetFuelForMass(int mass)
+        {
+            return (int)Math.Floor((decimal)mass / 3) - 2;
+        }
+
+        public static int GetTotalFuelRequirements(List<string> moduleMasses)
+        {
+            var totalMass = 0;
+
+            foreach (var moduleMass in moduleMasses)
+            {
+                totalMass += GetFuelRequiredForFuel(GetFuelForMass(int.Parse(moduleMass)));
+            }
+
+            return totalMass;
+        }
+
+        public static int GetFuelRequiredForFuel(int moduleMass)
+        {
+            var fuelRequired = (int)Math.Floor((decimal)moduleMass / 3) - 2;
+
+            if (fuelRequired <= 0)
+            {
+                fuelRequired = 0;
+            }
+            else
+            {
+                fuelRequired = GetFuelRequiredForFuel(fuelRequired);
+            }
+
+            return moduleMass + fuelRequired;
         }
     }
 }
