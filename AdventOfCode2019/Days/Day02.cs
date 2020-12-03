2f7fe56300bee51a0f5adf63198b98570da211ed
jniemetscheck <jniemetscheck@gmail.com>
2020-12-03 10:08:10 -0600

    2019 Day 2 Part 1

--- AdventOfCode2019/Days/Day02.cs
+++ AdventOfCode2019/Days/Day02.cs
@@ -24,17 +24,46 @@ namespace AdventOfCode2019.Days
 
         public static int GetPositionWhenProgramHalts(List<string> inputs)
         {
-            for (int position = 0; position < inputs.Count; position++)
-            {
-                var opCode = int.Parse(inputs[position]);
+            var position = 0;
+            var value = -1;
 
-                if (opCode == 1)
+            while (true)
+            {
+                if (inputs[position] == "99")
                 {
-                    //inputs[int.Parse(inputs[])];
+                    value = int.Parse(inputs[0]);
+                    break;
                 }
+                var instruction = new Instruction
+                {
+                    OpCode = int.Parse(inputs[position]),
+                    InputOnePosition = int.Parse(inputs[position + 1]),
+                    InputTwoPosition = int.Parse(inputs[position + 2]),
+                    OutputPosition = int.Parse(inputs[position + 3])
+                };
+
+                if (instruction.OpCode == 1)
+                {
+                    inputs[instruction.OutputPosition] = (int.Parse(inputs[instruction.InputOnePosition]) + int.Parse(inputs[instruction.InputTwoPosition])).ToString();
+                }
+                else if (instruction.OpCode == 2)
+                {
+                    inputs[instruction.OutputPosition] = (int.Parse(inputs[instruction.InputOnePosition]) * int.Parse(inputs[instruction.InputTwoPosition])).ToString();
+                }
+
+
+                position += 4;
             }
 
-            return 0;
+            return value;
         }
+    }
+
+    public class Instruction
+    {
+        public int OpCode { get; set; }
+        public int InputOnePosition { get; set; }
+        public int InputTwoPosition { get; set; }
+        public int OutputPosition { get; set; }
     }
 }
