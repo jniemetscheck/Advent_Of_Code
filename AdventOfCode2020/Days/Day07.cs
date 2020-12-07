ac6befe613b7e4a1c509e83e5a0c9ff80c746605
jniemetscheck <jniemetscheck@gmail.com>
2020-12-07 08:19:57 -0600

    2020 Day 7 Part 1

---
+++ AdventOfCode2020/Days/Day07.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2020.Days
+{
+    public static class Day07
+    {
+        private static readonly string FilePath = Directory.GetCurrentDirectory() + @"/Input/Day07.txt";
+
+        public static int GetResultPartOne()
+        {
+            var input = File.ReadAllLines(FilePath);
+
+            var rules = GetMappedBagRules(input.ToList());
+
+            return BagsThatContainAnotherBag("shiny gold", rules).Distinct().Count();
+        }
+
+        public static List<string> BagsThatContainAnotherBag(string anotherBag, List<BagRule> rules)
+        {
+            var bagColors = new List<string>();
+
+            var containingBags = rules.Where(r => r.ContainedBags.ContainsKey(anotherBag)).ToList();
+
+            foreach (var containingBag in containingBags)
+            {
+                bagColors.Add(containingBag.Description);
+                bagColors.AddRange(BagsThatContainAnotherBag(containingBag.Description, rules));
+            }
+
+            return bagColors;
+        }
+
+        public static List<BagRule> GetMappedBagRules(List<string> bagRules)
+        {
+            var result = new List<BagRule>();
+
+            foreach (var bagRule in bagRules)
+            {
+                var rule = new BagRule();
+
+                var bagRuleMainSplit = Regex.Split(bagRule, "contain");
+
+                rule.Description = bagRuleMainSplit[0].Trim().Split(" ".ToCharArray())[0] + " " + bagRuleMainSplit[0].Trim().Split(" ".ToCharArray())[1];
+                rule.ContainedBags = new Dictionary<string, int>();
+
+                var containedBagsSplit = bagRuleMainSplit[1].Trim().Split(",".ToCharArray());
+
+                foreach (var containedBagSplit in containedBagsSplit)
+                {
+                    var spaceSplit = containedBagSplit.Trim().Split(" ".ToCharArray());
+
+                    if (spaceSplit[0] == "no")
+                    {
+                        break;
+                    }
+
+                    //var containedBag = new ContainedBag
+                    //{
+                    //    Quantity = int.Parse(spaceSplit[0]), 
+                    //    Description = spaceSplit[1] + " " + spaceSplit[2]
+                    //};
+
+                    rule.ContainedBags.Add(spaceSplit[1] + " " + spaceSplit[2], int.Parse(spaceSplit[0]));
+                }
+
+                result.Add(rule);
+            }
+
+            return result;
+        }
+    }
+
+    public class BagRule
+    {
+        public string Description { get; set; }
+        public Dictionary<string, int> ContainedBags { get; set; }
+    }
+
+    public class ContainedBag
+    {
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+    }
+}
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -22,7 +22,10 @@ namespace AdventOfCode2020
             //Console.WriteLine($"Result for Day 04 Part 2: {Days.Day05.GetResultPartTwo()}");
 
             //Console.WriteLine($"Result for Day 06 Part 1: {Days.Day06.GetResultPartOne()}");
-            Console.WriteLine($"Result for Day 06 Part 2: {Days.Day06.GetResultPartTwo()}");
+            //Console.WriteLine($"Result for Day 06 Part 2: {Days.Day06.GetResultPartTwo()}");
+
+            Console.WriteLine($"Result for Day 07 Part 1: {Days.Day07.GetResultPartOne()}");
+            //Console.WriteLine($"Result for Day 07 Part 2: {Days.Day07.GetResultPartTwo()}");
 
             Console.ReadLine();
         }
