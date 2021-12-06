dd5384e1578b9bcfcca548b26c92c6dd2d16f937
jniemetscheck <jniemetscheck@gmail.com>
2021-12-06 07:08:42 -0600

    2021 infrastructure

---
+++ AdventOfCode2021/Days/Day01.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2021.Days
+{
+    public class Day01
+    {
+        private static readonly string FilePath = Directory.GetCurrentDirectory() + @"/Input/Day01.txt";
+
+        public static int GetResultPartOne()
+        {
+            var lines = File.ReadAllLines(FilePath);
+
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Result for Day 01 Part 1: {Days.Day01.GetResultPartOne()}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
