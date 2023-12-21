b3f1c0c34fb39dae68d0beb21ed63c5a5a680db0
Justin Niemetscheck <jniemetscheck@gmail.com>
2023-12-21 07:34:42 -0600

    2021
    * Day 1
    
    * day 1 part 2

--- AdventOfCode2021/Days/Day01.cs
+++ AdventOfCode2021/Days/Day01.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace AdventOfCode2021.Days
@@ -11,7 +12,65 @@ namespace AdventOfCode2021.Days
         {
             var lines = File.ReadAllLines(FilePath);
 
-            return 0;
+            return GetIncreaseNumberTimes(lines.ToList());
+        }
+
+        public static int GetResultPartTwo()
+        {
+            var lines = File.ReadAllLines(FilePath);
+
+            return GetIncreasedTimesWithStatic(lines.ToList());
+        }
+
+        public static int GetIncreaseNumberTimes(List<string> measurements)
+        {
+            var increasedCount = 0;
+            var previous = 0;
+            for (var i = 0; i < measurements.Count; i++)
+            {
+                if (i == 0)
+                {
+                    previous = int.Parse(measurements[i]);
+                    continue;
+                }
+
+                if (previous < int.Parse(measurements[i]))
+                {
+                    increasedCount++;
+                }
+
+                previous = int.Parse(measurements[i]);
+            }
+
+            return increasedCount;
+        }
+
+        public static int GetIncreasedTimesWithStatic(List<string> measurements)
+        {
+            var increasedCount = 0;
+            var previous = 0;
+            for (var i = 0; i < measurements.Count; i++)
+            {
+                if (i == 0)
+                {
+                    previous = int.Parse(measurements[i]) + int.Parse(measurements[i + 1]) + int.Parse(measurements[i + 2]);
+                    continue;
+                }
+
+                if (i >= measurements.Count - 2)
+                {
+                    break;
+                }
+
+                if (previous < int.Parse(measurements[i]) + int.Parse(measurements[i + 1]) + int.Parse(measurements[i + 2]))
+                {
+                    increasedCount++;
+                }
+
+                previous = int.Parse(measurements[i]) + int.Parse(measurements[i + 1]) + int.Parse(measurements[i + 2]);
+            }
+
+            return increasedCount;
         }
     }
 }
